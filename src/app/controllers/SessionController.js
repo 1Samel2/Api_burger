d9a178ef84122b32c10d854ba3d0ca00da6bcbf8
Samuel <samuelsguedes@hotmail.com>
2022-11-16 16:29:57 -0300

    Saving progress

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,12 +1,40 @@
 import * as Yup from 'yup'
+import User from '../models/User'
 
 class SessionController {
-    store(request, response){
+    async store(request, response) {
         const schema = Yup.object().shape({
-            email: Yup.string.email().required(),
+            email: Yup.string().email().required(),
             password: Yup.string().required(),
         })
 
-        
+        const userEmailOrPasswordIncorrect = () => {
+            return response
+                .status(401)
+                .json({ error: 'Mare sure your password or email are correct' })
+        }
+
+        if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
+
+        const { email, password } = request.body
+
+        const user = await User.findOne({
+            where: { email },
+        })
+
+        if (!user) userEmailOrPasswordIncorrect()
+
+        if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
+
+        return response.json({
+            id: user.id,
+            email,
+            name: user.name,
+            admin: user.admin,
+        })
     }
 }
+
+
+
+export default new SessionController()
