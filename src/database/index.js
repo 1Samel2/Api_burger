5ffb628c6317cc61e9fe56b4922ab20de80ed34e
samuel <samuelsguedes@hotmail.com>
2022-12-19 14:46:52 -0300

    arrumando erro de produtos

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,26 @@
-import Sequelize from "sequelize";
-import Product from "../app/models/Product";
-import User from "../app/models/User";
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+import Product from '../app/models/Product';
+import User from '../app/models/User';
+import Category from '../app/models/Category';
 
-import configDatabase from "../config/database";
+import configDatabase from '../config/database';
 
-const models = [User, Product];
+const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(configDatabase);
     models.map((model) => model.init(this.connection));
   }
+  mongo() {
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/codeburger', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
 }
-
 export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
-import { Router } from 'express'
-import multer from 'multer'
-import multerConfig from './config/multer'
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import ProductController from './app/controllers/ProductController'
-import UserController from './app/controllers/UserController'
-import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+const upload = multer(multerConfig);
 
-const upload = multer(multerConfig)
+const routes = new Router();
 
-const routes = new Router()
-
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
-routes.post('/products', upload.single('file'), ProductController.store)
-routes.get('/products', ProductController.index)
-export default routes
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.post('/products', upload.single('file'), ProductController.store);
+routes.get('/products', ProductController.index);
+export default routes;
