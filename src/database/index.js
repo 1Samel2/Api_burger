ffa9dbfc5d2c171d51cc7ee324baa52dd171355e
samuel <samuelsguedes@hotmail.com>
2022-12-19 18:25:00 -0300

    Saving progress

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import mongoose from 'mongoose';
+import mongoose, { model } from 'mongoose';
 import Product from '../app/models/Product';
 import User from '../app/models/User';
 import Category from '../app/models/Category';
@@ -10,17 +10,19 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
-    this.mongo();
+   
   }
   init() {
     this.connection = new Sequelize(configDatabase);
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
   }
-  mongo() {
+  /* mongo() {
     this.mongoConnection = mongoose.connect('mongodb://localhost:27017/codeburger', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-  }
+  } */
 }
 export default new Database();
